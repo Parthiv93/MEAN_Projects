9aaaf2cd365901d4cf09dfec569b63a365ca52d4
Parthiv93 <parthivmanoj93@gmail.com>
2024-06-12 14:17:14 +0530

    Added HTTP Methods

--- VideoPlayer/ngApp/server/routes/api.js
+++ VideoPlayer/ngApp/server/routes/api.js
@@ -1,8 +1,73 @@
 const express=require('express');
 const router=express.Router();
+const mongoose=require('mongoose');
+const Video=require('../models/video');
 
-router.get('/',(req,res)=>{
-    res.send('api works');
-});
+const db= "mongodb+srv://parthivm93:2kzVLswQSUT4Gyfy@s3filetracker.aokysux.mongodb.net/videoplayer?retryWrites=true&w=majority&ssl=true"
+
+mongoose.connect(db)
+.then(() => {
+    console.log('Database connection successful');
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+  });
+
+  router.get('/videos', async (req, res) => {
+    try {
+      const videos = await Video.find({});
+      res.json(videos);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.get('/videos/:id', async (req, res) => {
+    try {
+      const video = await Video.findById(req.params.id);
+      res.json(video);
+    } catch (err) {
+      res.status(500).send(err); 
+    }
+  });
+
+  router.post('/videos', async (req, res) => {
+    try {
+      const video = new Video();
+      video.title = req.body.title;
+      video.url = req.body.url;
+      video.description = req.body.description;
+      await video.save();
+      res.json(video);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.put('/videos/:id', async (req, res) => {
+    try {
+      const video = await Video.findByIdAndUpdate
+      (req.params.id, 
+        {
+          $set: { title: req.body.title, url: req.body.url, description: req.body.description }
+        }, 
+        { upsert: true }
+      );
+      res.json(video);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.delete('/videos/:id', async (req, res) => {
+    try {
+      const video = await Video.findByIdAndRemove(req.params.id);
+      res.json(video);
+    } catch (err) {
+      res.status(500).send
+    }
+  });
+
+  
 
 module.exports=router;
