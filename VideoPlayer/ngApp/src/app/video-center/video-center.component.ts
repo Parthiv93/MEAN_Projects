e49230d41c5bad7852e24094e29fd578d3c134f7
Parthiv93 <parthivmanoj93@gmail.com>
2024-06-14 11:41:22 +0530

    Created video class

--- VideoPlayer/ngApp/src/app/video-center/video-center.component.ts
+++ VideoPlayer/ngApp/src/app/video-center/video-center.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Video } from '../video';
 
 @Component({
   selector: 'app-video-center',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-center.component.css']
 })
 export class VideoCenterComponent implements OnInit {
+
+  videos: Video[] = [
+    {'_id': '1', 'title': 'Title 1', 'url': 'URL 1', 'description': 'Description 1'},
+    {'_id': '2', 'title': 'Title 2', 'url': 'URL 2', 'description': 'Description 2'},
+    {'_id': '3', 'title': 'Title 3', 'url': 'URL 3', 'description': 'Description 3'},
+    {'_id': '4', 'title': 'Title 4', 'url': 'URL 4', 'description': 'Description 4'}
+  ];
 
   constructor() { }
 
--- VideoPlayer/ngApp/src/app/video-list/video-list.component.ts
+++ VideoPlayer/ngApp/src/app/video-list/video-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Video } from '../video'; // Import the Video interface or class if it exists
 
 @Component({
   selector: 'video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
-export class VideoListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class VideoListComponent {
+  @Input() videos: Video[] = [];
 }
