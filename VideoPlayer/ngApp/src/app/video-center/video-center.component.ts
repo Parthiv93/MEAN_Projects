be371b97005645b19227a5966b1987ac3c0f632d
Parthiv93 <parthivmanoj93@gmail.com>
2024-06-20 17:42:21 +0530

    Added newVideo method

--- VideoPlayer/ngApp/src/app/video-center/video-center.component.ts
+++ VideoPlayer/ngApp/src/app/video-center/video-center.component.ts
@@ -9,7 +9,7 @@ import { VideoService } from '../video.service';
 })
 export class VideoCenterComponent implements OnInit {
 
-  videos: Video[] = []; // Initialize as an empty array
+  videos: Video[] = []; 
   selectedVideo!: Video;
 
   constructor(private _videoService: VideoService) { }
@@ -18,7 +18,7 @@ export class VideoCenterComponent implements OnInit {
     this._videoService.getVideos()
       .subscribe(
         resVideoData => {
-          console.log('Video data fetched:', resVideoData); // Add logging
+          console.log('Video data fetched:', resVideoData); 
           this.videos = resVideoData;
         },
         err => {
@@ -30,5 +30,27 @@ export class VideoCenterComponent implements OnInit {
   onSelectVideo(vid: Video): void {
     this.selectedVideo = vid;
     console.log(this.selectedVideo);
+  }
+
+  onSubmitAddVideo(vid: Video): void {
+    this.videos.push(vid);
+    this._videoService.addVideo(vid)
+      .subscribe(
+        resNewVideo => {
+          this.selectedVideo = resNewVideo;
+        },
+        err => {
+          console.error('Error adding video:', err);
+        }
+      );
+  }
+
+  newVideo(): void {
+    this.selectedVideo = {
+      _id: '',
+      title: '',
+      url: '',
+      description: ''
+    };
   }
 }
--- VideoPlayer/ngApp/src/app/video.service.ts
+++ VideoPlayer/ngApp/src/app/video.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Video } from './video';
 
@@ -8,9 +8,16 @@ import { Video } from './video';
 })
 export class VideoService {
   private _getUrl = '/api/videos';
+  private _postUrl = '/api/video';
   constructor(private _http: HttpClient) { }
 
   getVideos(): Observable<Video[]> {
     return this._http.get<Video[]>(this._getUrl);
+  }
+
+  addVideo(video: Video): Observable<Video> {
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    let options = { headers: headers };
+    return this._http.post<Video>(this._postUrl, JSON.stringify(video), options);
   }
 }
